632ffa3d0a8968b8f18584219393574d75447188
Stefania Mardar <63240946+fiamardar@users.noreply.github.com>
2021-11-16 12:58:47 +0200

    Lab 5 Grammar Class + Production + Main + g1.txt + Documentation

---
+++ Lab 4/Main_Grammar.py
@@ -0,0 +1,40 @@
+from Grammar import Grammar
+
+
+def print_menu():
+    print("\nFor displaying the elements of the FA in file, press: \n"
+          "0. Exit\n"
+          "1. Set of nonterminals\n"
+          "2. Set of terminals\n"
+          "3. Set of productions\n"
+          "4. Productions for a given nonterminal\n"
+          "5. CFG check\n")
+
+
+if __name__ == '__main__':
+
+    grammar = Grammar("g1.txt")
+
+    while True:
+        print_menu()
+        result = int(input())
+
+        if result == 1:
+            print("The set of nonterminals: ", grammar.set_of_nonterminals)
+        elif result == 2:
+            print("The set of terminals: ", grammar.set_of_terminals)
+        elif result == 3:
+            print("The set of productions: ")
+            for production in grammar.set_of_productions:
+                print(production)
+        elif result == 4:
+            print("Enter the nonterminal: ")
+            nonterminal = input()
+            print("The productions for the given nonterminal are: ")
+            print(grammar.get_productions_for_nonterminal(nonterminal))
+        elif result == 5:
+            print("Grammar is CFG: ", grammar.cfg_check())
+        elif result == 0:
+            break
+        else:
+            print("Incorrect value! Please try again")
